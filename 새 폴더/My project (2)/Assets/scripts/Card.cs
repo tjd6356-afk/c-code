22bfa0de3e10b0538e31c8d75c21b63429026330
tjd6356-afk <tjd6356@gmail.com>
2026-03-31 18:30:52 +0900

    adadadadff

--- 새 폴더/My project (2)/Assets/scripts/Card.cs
+++ 새 폴더/My project (2)/Assets/scripts/Card.cs
@@ -5,20 +5,40 @@ public class Cards : MonoBehaviour
 {
         public TextMeshProUGUI text;
         public int cardNumber;
+        public float RotationSpeed = 10.0f;
+        public bool isClick = false;
+        private Quaternion flipRotation = Quaternion.Euler(0, 180f, 0);
+        private Quaternion originRotation = Quaternion.Euler(0, 0, 0);
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         text = GetComponentInChildren<TextMeshProUGUI>();
         
-        text.text = Random.Range(0, 10).ToString();
+        cardNumber = Random.Range(0, 10);
+
+        text.text = cardNumber.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // transform.Rotate(0, 2, 0);
+        
+        if(isClick)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, RotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, RotationSpeed * Time.deltaTime);
+        }
+        
+        
     }
 
-    
+    public void ClickCard()
+    {
+        isClick = !isClick;
+    }
+
 }
