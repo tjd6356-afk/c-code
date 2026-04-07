bf15d92db473a0302f1d29a8b978546f313906fa
tjd6356-afk <tjd6356@gmail.com>
2026-04-07 19:05:01 +0900

    adadad

--- 새 폴더/My project (2)/Assets/scripts/Card.cs
+++ 새 폴더/My project (2)/Assets/scripts/Card.cs
@@ -1,36 +1,32 @@
 using UnityEngine; 
 using TMPro;
+using UnityEngine.UI;
 
-public class Cards : MonoBehaviour
+public class Card : MonoBehaviour
 {
         public TextMeshProUGUI text;
         public int cardNumber;
         public float RotationSpeed = 10.0f;
-        public bool isClick = false;
+        public bool isFront = true;
+        public bool isMatched = false;
         private Quaternion flipRotation = Quaternion.Euler(0, 180f, 0);
         private Quaternion originRotation = Quaternion.Euler(0, 0, 0);
+        public CardGame cardGame;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        text = GetComponentInChildren<TextMeshProUGUI>();
-        
-        cardNumber = Random.Range(0, 10);
 
-        text.text = cardNumber.ToString();
-    }
 
     // Update is called once per frame
     void Update()
     {
         
-        if(isClick)
+        if(isFront)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, RotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, RotationSpeed * Time.deltaTime);
         }
         else
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, RotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, RotationSpeed * Time.deltaTime);
         }
         
         
@@ -38,7 +34,21 @@ public class Cards : MonoBehaviour
 
     public void ClickCard()
     {
-        isClick = !isClick;
+        if(!isMatched)
+        {
+            cardGame.OnClickCard(this);
+            isFront = !isFront;
+        }
     }
 
+    public void SetCardNumber(int newNumber)
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        cardNumber = newNumber;
+        text.text = newNumber.ToString();
+    }
+    public void ChangeColor(Color newColor)
+    {
+        GetComponent<Image>().color = newColor;
+    }
 }
--- 새 폴더/My project (2)/Assets/scripts/CardGame.cs
+++ 새 폴더/My project (2)/Assets/scripts/CardGame.cs
@@ -1,16 +1,97 @@
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class CardGame : MonoBehaviour
 {
+    public List<Card> card;
+    private Card firstCard = null;
+    private Card secondCard = null;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartGame();
     }
 
-    // Update is called once per frame
-    void Update()
+    List<int> GeneratePairNumbers(int cardCount)
     {
-        
+        int pairCount = cardCount / 2;
+        List<int> newCardNumbers = new List<int>();
+
+        for(int i = 0; i < pairCount; ++i)
+        {
+            newCardNumbers.Add(i);
+            newCardNumbers.Add(i);
+        }
+
+        for (int i = newCardNumbers.Count - 1; i > 0; i--)
+        {
+            int rnd = Random.Range(0, i + 1);
+            int temp = newCardNumbers[i];
+            newCardNumbers[i] = newCardNumbers[rnd];
+            newCardNumbers[rnd] = temp;
+        }
+
+        return newCardNumbers;
+
     }
+
+    private void StartGame()
+    {
+        List<int> randomPairNumbers = GeneratePairNumbers(card.Count);
+
+        for(int i = 0; i < card.Count; ++i)
+        {
+            card[i].SetCardNumber(randomPairNumbers[i]);
+            card[i].isFront = false;
+        }
+    }
+
+    private void CheckCard()
+    {
+        if(firstCard.cardNumber == secondCard.cardNumber)
+        {
+            firstCard.isMatched = true;
+            secondCard.isMatched = true;
+
+            firstCard.ChangeColor(Color.magenta);
+            secondCard. ChangeColor(Color.magenta);
+
+            firstCard = null;
+            secondCard = null;
+        }
+        else
+        {
+            Invoke("HideCard", 1.0f);
+        }
+    }
+    
+    private void HideCard()
+    {
+        firstCard.isFront = false;
+        secondCard.isFront = false;
+
+        firstCard = null;
+        secondCard = null;
+    }
+
+    public void OnClickCard(Card card)
+    {
+        if (firstCard == null)
+        {
+            firstCard = card;
+        }
+
+        else
+        {
+            secondCard = card;
+        }
+
+        if (firstCard != null && secondCard != null)
+        {
+            CheckCard();
+        }
+    }
+    
 }
