6b45df313a156a4f2d710176f244087e7f67ba7d
tjd6356-afk <tjd6356@gmail.com>
2026-04-27 22:48:25 +0900

    gtgkgyui

--- 새 폴더/My project (2)/Assets/scripts/Card.cs
+++ 새 폴더/My project (2)/Assets/scripts/Card.cs
@@ -9,55 +9,97 @@ public class Card : MonoBehaviour
         public float RotationSpeed = 10.0f;
         public bool isFront = true;
         public bool isMatched = false;
+
+        [Header("Card Images")]
+        public Sprite backSprite;     // 인스펙터에서 넣을 카드 뒷면 이미지
+        private Sprite frontSprite;   // 게임 시작 시 할당받을 앞면 이미지
+
         private Quaternion flipRotation = Quaternion.Euler(0, 180f, 0);
         private Quaternion originRotation = Quaternion.Euler(0, 0, 0);
-        public CardGame cardGame;
+        private Color originalColor;
+        private Image cardImage;
+
+        [HideInInspector] public CardGame cardGame;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
 
+    private void Awake()
+    {
+        cardImage = GetComponent<Image>();
+        originalColor = cardImage.color;
+
+        if (text == null) text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
-        if(isFront)
+
+        // 1. 부드러운 회전 처리
+        Quaternion targetRotation = isFront ? originRotation : flipRotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+
+        // 2. 현재 카드의 Y축 회전 각도를 가져옵니다. (0 ~ 360도)
+        float currentYAngle = transform.eulerAngles.y;
+
+        // 3. 각도가 90도 ~ 270도 사이라면 카드가 뒤집힌 상태로 간주
+        if (currentYAngle > 90f && currentYAngle < 270f)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, originRotation, RotationSpeed * Time.deltaTime);
+            // 뒷면 보여주기
+            if (cardImage != null && backSprite != null)
+                cardImage.sprite = backSprite;
+
+            // 텍스트(숫자) 숨기기
+            if (text != null)
+                text.enabled = false;
         }
         else
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, flipRotation, RotationSpeed * Time.deltaTime);
+            // 앞면 보여주기
+            if (cardImage != null && frontSprite != null)
+                cardImage.sprite = frontSprite;
+
+            // 텍스트(숫자) 보이기
+            if (text != null)
+                text.enabled = true;
         }
-        
-        
+
+
     }
 
     public void ClickCard()
     {
-        if(!isMatched)
+        if(!isMatched && cardGame != null)
         {
             cardGame.OnClickCard(this);
         }
     }
 
-    public void Flip(bool isFront)
+    public void Flip(bool front)
     {
-        this.isFront = isFront;
+        isFront = front;
     }
 
     public void SetCardNumber(int newNumber)
     {
-        text = GetComponentInChildren<TextMeshProUGUI>();
         cardNumber = newNumber;
-        text.text = newNumber.ToString();
+        if (text != null) text.text = newNumber.ToString();
     }
+
     public void ChangeColor(Color newColor)
     {
-        GetComponent<Image>().color = newColor;
+        if (cardImage != null) cardImage.color = newColor;
+    }
+
+    public void ResetColor()
+    {
+        if (cardImage != null) cardImage.color = originalColor;
     }
 
     public void SetImage(Sprite sprite)
     {
-        GetComponent<Image>().sprite = sprite;
+        // 전달받은 이미지를 '앞면 이미지'로 저장해 둡니다.
+        frontSprite = sprite;
     }
 }
--- 새 폴더/My project (2)/Assets/scripts/CardGame.cs
+++ 새 폴더/My project (2)/Assets/scripts/CardGame.cs
@@ -1,11 +1,18 @@
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardGame : MonoBehaviour
 {
-    public List<Card> card;
+    [Header("Settings")]
+    public GameObject cardPrefab;    // 카드 프리팹
+    public Transform cardParent;     // 카드가 생성될 부모 객체 (Grid Layout Group 권장)
+    public int totalCardCount = 12;  // 생성할 카드 총 개수 (2~20 사이)
+
+    public List<Card> card = new List<Card>(); // 생성된 카드들을 담을 리스트
     public List<Sprite> sprites;
+
     private Card firstCard = null;
     private Card secondCard = null;
     private bool isChecking = false;
@@ -13,6 +20,37 @@ public class CardGame : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        SetupBoard(totalCardCount);
+    }
+
+    public void SetupBoard(int count)
+    {
+        // 1. 기존 카드 제거 (초기화용)
+        foreach (Card c in card)
+        {
+            if (c != null) Destroy(c.gameObject);
+        }
+        card.Clear();
+
+        // 2. 홀수라면 짝수로 맞춤 (페어 게임이므로)
+        if (count % 2 != 0) count--;
+        if (count < 2) count = 2;
+        if (count > 20) count = 20;
+
+        totalCardCount = count;
+
+        // 3. 프리팹 생성
+        for (int i = 0; i < totalCardCount; i++)
+        {
+            GameObject newCardObj = Instantiate(cardPrefab, cardParent);
+            Card newCard = newCardObj.GetComponent<Card>();
+
+            // 카드 스크립트에 이 게임 매니저 정보를 전달
+            newCard.cardGame = this;
+            card.Add(newCard);
+        }
+
+        // 4. 게임 시작 로직 실행
         StartGame();
     }
 
@@ -46,7 +84,11 @@ public class CardGame : MonoBehaviour
         for(int i = 0; i < card.Count; ++i)
         {
             card[i].SetCardNumber(randomPairNumbers[i]);
-            card[i].SetImage(sprites[randomPairNumbers[i]]);
+            // 이미지 스프라이트 개수가 부족하지 않은지 체크 필요
+            if (randomPairNumbers[i] < sprites.Count)
+            {
+                card[i].SetImage(sprites[randomPairNumbers[i]]);
+            }
             card[i].isFront = false;
         }
     }
